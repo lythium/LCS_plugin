2a3ff2124d94be8306b09a644e09a8757feaa03b
lythium <niko.phoenix1@gmail.com>
2017-06-16 00:09:27 +0200

    update bo update page

--- views/admin/bo-update-lcs.php
+++ views/admin/bo-update-lcs.php
@@ -1,11 +1,15 @@
 <?php
-$results = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}lcs_category");
-if ($results):
-	foreach ($results as $key ):
-		$id = $key->Slider_id;
-		$name = $key->Slider_Name;
-		$categories_id = unserialize($key->Category_ID);
-		echo '<span>ID : '. $id .' - Name : '. $name .'</span>';
-	endforeach;
+if (empty($_POST["select_id"]))
+	header('Location: '. admin_url("admin.php?page=lcs").')';
+else :
+	$results = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}lcs_category");
+	if ($results):
+		foreach ($results as $key ):
+			$id = $key->Slider_id;
+			$name = $key->Slider_Name;
+			$categories_id = unserialize($key->Category_ID);
+			echo '<span>ID : '. $id .' - Name : '. $name .'</span>';
+		endforeach;
+	endif;
 endif;
  ?>
