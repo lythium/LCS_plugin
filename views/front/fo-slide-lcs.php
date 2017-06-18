f1b1baa5490b7b19377cfeb51fca1699b98da8c7
lythium <niko.phoenix1@gmail.com>
2017-06-18 14:01:23 +0200

    update front slide

--- views/front/fo-slide-lcs.php
+++ views/front/fo-slide-lcs.php
@@ -1,28 +1,33 @@
 <?php
 echo '<div class="row slide-container-lcs lcs_' . $id_shortcode . '">';
 	echo '<div class="slide-in-container-lcs">';
-	$categories_id = unserialize($key->Category_ID);
-	foreach ( $categories_id as $cat_id ):
-		$cat_name = get_cat_name( $cat_id );
-		$posts_last = get_posts(array("cat" => $cat_id, "showposts" => 1));
-		$id_last = $posts_last[0]->ID;
+		$width = "400%";
+		echo '<ul style="width:'.$width.';">';
+		$categories_id = unserialize($key->Category_ID);
+		foreach ( $categories_id as $cat_id ):
+			$cat_name = get_cat_name( $cat_id );
+			$posts_last = get_posts(array("cat" => $cat_id, "showposts" => 1));
+			$id_last = $posts_last[0]->ID;
 
-		echo '<div class="lcs-slide-part row">';
-		if ( has_post_thumbnail( $id_last ) ):
-			echo get_the_post_thumbnail( $id_last, 'thumbnail', array( 'class' => 'col-md-12' ) );
-		else :
-			echo '<img class="col-md-12" src="'.plugin_dir_url( __FILE__ ).'default.jpg">';
-		endif;
-		echo '</div>';
+			echo '<li class="SlidePart row">';
+				echo '<a class="card-lcs-link" href="' . get_category_link($cat_id) . '">';
+					if ( has_post_thumbnail( $id_last ) ):
+						echo get_the_post_thumbnail( $id_last, 'thumbnail', array( 'class' => 'col-md-12' ) );
+					else :
+						echo '<img class="col-md-12" src="'.plugin_dir_url( __FILE__ ).'default.jpg">';
+					endif;
+				echo'</a>';
+			echo '</li>';
 
-	endforeach;
+		endforeach;
+		echo '</ul>';
 
 		echo '<div class="slide-lcs-nav .lcs-white lcs-section">';
 			echo '<div class="lcs-left" onclick="plusDivs(-1)">❮</div>';
 			echo '<div class="lcs-right" onclick="plusDivs(1)">❯</div>';
 			for ($i=1; $i < 4 ; $i++):
-				echo '<span class="lcs-badge span-lcs lcs-white-hover" onclick="currentDiv('.$i.')"></span>';
-				echo '';
+				echo '<span class="lcs-badge span-lcs lcs-hover-white" onclick="currentDiv('.$i.')"></span>';
+				echo ' ';
 			endfor;
 		echo '</div>';
 
@@ -34,33 +39,14 @@ echo '</div>';
 <script>
 	$=jQuery.noConflict();
 	$(document).ready(function() {
-		var slideIndex = 1;
-		showDivs(slideIndex);
-
-		function plusDivs(n) {
-		  showDivs(slideIndex += n);
-		}
-
-		function currentDiv(n) {
-		  showDivs(slideIndex = n);
-		}
-
-		function showDivs(n) {
-		  var i;
-		  var x = document.getElementsByClassName("lcs-slide-part");
-		  var dots = document.getElementsByClassName("span-lcs");
-		  if (n > x.length) {slideIndex = 1}
-		  if (n < 1) {slideIndex = x.length}
-		  for (i = 0; i < x.length; i++) {
-		     x[i].style.display = "none";
-		  }
-		  for (i = 0; i < dots.length; i++) {
-		     dots[i].className = dots[i].className.replace("lcs-white", "");
-		  }
-		  x[slideIndex-1].style.display = "block";
-		  dots[slideIndex-1].className += "lcs-white";
-	  };
-  });
+		$(function(){
+			setInterval(function(){
+				$(".slide-in-container-lcs ul li:first-child").animate({"margin-left": -350}, 800, function(){
+				 $(this).css("margin-left",0).appendTo(".slide-in-container-lcs ul");
+				});
+			}, 3500);
+		});
+	});
 </script>
 
 <style media="screen">
@@ -68,24 +54,29 @@ echo '</div>';
 
 	}
 	.slide-in-container-lcs {
-		max-width: 800px;
-		margin: 10 auto;
-		position: relative;
+		max-width: 600px;
+		margin: 10px auto!important;
+		overflow: hidden;
+		border: 3px solid #F2F2F2;
 	}
-	.lcs-slide-part {
-		width: 100%;
-		display: block;
+	.slide-in-container-lcs ul {
+		padding:0; margin:0;
+		list-style: none;
+	}
+	.SlidePart {
+		float: left;
 		margin: 0px;
 	}
 	.slide-lcs-nav {
 		width: 100%;
 		text-align: center;
-		font-size: 18px !important;
+		font-size: 18px!important;
 		position: absolute;
 		left: 50%;
 		bottom: 0;
 		transform: translate(-50%,0%);
 		-ms-transform: translate(-50%,0%);
+		padding: 0.01em 16px;
 	}
 	.lcs-left {
 		float: left!important;
@@ -96,16 +87,17 @@ echo '</div>';
 		cursor: pointer;
 	}
 	.lcs-white {
-		color: #ffffff;
+		color: #000!important;
+		background-color: #ffffff!important;
 	}
-	.lcs-white-hover {
+	.lcs-hover-white {
 		color: #ffffff;
 	}
 	.lcs-badge {
 		display: inline-block;
 		border-radius: 50%;
-		border: 1px solid #ccc !important;
-		background-color: transparent !important;
+		border: 1px solid #cccccc!important;
+		background-color: transparent;
 		height: 13px;
 		width: 13px;
 		padding: 0;
@@ -115,5 +107,21 @@ echo '</div>';
 	.lcs-section {
 		margin-top: 16px !important;
 		margin-bottom: 16px !important;
+	}
+	.card-lcs-link {
+		display: block;
+		width: 100%;
+		height: 56.25%;
+		border-radius: 10px;
+		overflow: hidden;
+		border: 3px outset #949494;
+		box-shadow: 0px 0px 5px #555555, 0px 0px 10px #555555;
+	}
+	.card-lcs-link img {
+		padding: 0px;
+		width: 100%;
+		height: auto;
+		max-width: 100%;
+		max-height: 300px;
 	}
  </style>
