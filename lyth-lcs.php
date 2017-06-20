6fc43218fbe16332453e3c17280c399ea77d7e53
lythium <niko.phoenix1@gmail.com>
2017-06-20 20:08:53 +0200

    bugfix include css and js

--- lyth-lcs.php
+++ lyth-lcs.php
@@ -11,25 +11,28 @@ License:
 
 class Lyth_CS_Plugin
 {
-  public function __construct()
-  {
-    $this->version = $version;
+
+	public function __construct()
+	{
+
 		add_action('admin_menu', array($this, 'add_admin_menu'));
 		include_once plugin_dir_path( __FILE__ ).'class/class-add-lcs.php';
 		new Add_LCS();
 		include_once plugin_dir_path( __FILE__ ).'class/class-shortcode-lcs.php';
-	  new Shortcode_LCS();
+		new Shortcode_LCS();
 		include_once plugin_dir_path( __FILE__ ).'class/class-update-lcs.php';
 		new Update_LCS();
-    add_action( 'wp_enqueue_scripts', $this->enqueue_lcs_styles() );
-    add_action( 'wp_enqueue_scripts', $this->enqueue_lcs_scripts() );
+		add_action( 'wp_enqueue_scripts', array( __CLASS__, 'enqueue_lcs_styles' ) );
+		add_action( 'wp_enqueue_scripts', array( __CLASS__, 'enqueue_lcs_scripts' ) );
+	    // add_action( 'wp_enqueue_scripts', $this->enqueue_lcs_styles() );
+	    // add_action( 'wp_enqueue_scripts', $this->enqueue_lcs_scripts() );
 		register_activation_hook(__FILE__, array('Add_LCS', 'install'));
 		register_uninstall_hook(__FILE__, array('Add_LCS', 'uninstall'));
   }
 
 	public function add_admin_menu()
 	{
-	  add_menu_page('Category slide Plugin', 'LCS plugin', 'manage_options', 'lcs', array($this, 'menu_html'));
+	 	add_menu_page('Category slide Plugin', 'LCS plugin', 'manage_options', 'lcs', array($this, 'menu_html'));
 		add_submenu_page('lcs', 'Apercu des Slide', 'Apercu des Slide', 'manage_options', 'lcs', array($this, 'menu_html'));
 
 	}
@@ -39,27 +42,18 @@ class Lyth_CS_Plugin
 		include_once plugin_dir_path( __FILE__ ).'views/admin/bo-display-all-lcs.php';
 	}
 
-  public function enqueue_lcs_styles()
-  {
-    wp_enqueue_script(
-            'lcs_style',
-            plugin_dir_url( __FILE__ ) . 'css/lcs_style.css',
-            array(),
-            $this->version,
-            FALSE
-        );
-  }
+ 	public static function enqueue_lcs_styles()
+	{
+		$css_file = plugins_url( 'css/lcs_style.css', __FILE__ );
+		wp_enqueue_style( 'lcs_style', $css_file, false, "0.1" );
 
-  public function enqueue_lcs_scripts()
-  {
-    wp_enqueue_script(
-            'lcs_style',
-            plugins_url( '/js/demo.js', __FILE__ ),
-            array('jquery'),
-            $this->version,
-            FALSE
-          );
-  }
+	}
+
+	public static function enqueue_lcs_scripts()
+	{
+		$js_file = plugins_url( '/js/demo.js', __FILE__ );
+		wp_enqueue_script('lcs_style', $js_file, array('jquery'),false , "0.1");
+	}
 
 
 }
