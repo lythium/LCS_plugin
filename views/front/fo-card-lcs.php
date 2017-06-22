d382a7e735eadc48b3c2a0b9ff81b35c5e493af3
JazZ <aleber@team-ever.com>
2017-06-22 22:48:01 +0400

    fix FO card template

--- views/front/fo-card-lcs.php
+++ views/front/fo-card-lcs.php
@@ -1,36 +1,33 @@
-<?php
-echo '<div class="row card-container-lcs lcs_' . $id_shortcode . '">';
-	echo '<div class="card-in-container-lcs">';
-	$categories_id = unserialize($key->Category_ID);
-	foreach ( $categories_id as $cat_id ):
-		$cat_name = get_cat_name( $cat_id );
-		echo '<div class="'.esc_attr($col_size).' card_lcs">';
-			echo '<div class="inter-card-lcs">';
-				echo '<div class="in-card_lcs">';
-					echo '<div class="category_name_lcs inline text-center">';
-						echo '<a class="" href="' . get_category_link($cat_id) . '">';
-							echo '<h3>' . substrwords($cat_name, 15) . '</h3>';
-						echo'</a>';
-					echo '</div>';
-					echo '<div class="col-md-12 img-card-lcs">';
-						$posts_last = get_posts(array("cat" => $cat_id, "showposts" => 1));
-						$id_last = $posts_last[0]->ID;
-						echo '<div class="card-lcs-last-post">';
-							echo '<a class="card-lcs-link" href="' . get_category_link($cat_id) . '">';
-								if ( has_post_thumbnail( $id_last ) ):
-									echo get_the_post_thumbnail( $id_last, 'thumbnail', array( 'class' => 'col-md-12' ) );
-								else :
-									echo '<img class="col-md-12" src="'.plugin_dir_url( __FILE__ ).'default.jpg">';
-								endif;
-							echo'</a>';
-						echo '</div>';
-					echo '</div>';
-				echo '</div>';
-			echo '</div>';
-		echo '</div>';
-	endforeach;
-	echo '</div>';
-echo '</div>';
-
-
- ?>
+<div class="row card-container-lcs lcs_<?= $id_shortcode ?>">
+    <div class="card-in-container-lcs">
+    <?php $categories_id = unserialize($key->Category_ID); ?>
+    <?php foreach ($categories_id as $cat_id): ?>
+        <div class="<?= esc_attr($col_size) ?> card_lcs">
+            <div class="inter-card-lcs">
+                <div class="in-card_lcs">
+                    <div class="category_name_lcs inline text-center">
+                        <a class="" href="<?= get_category_link($cat_id) ?>">
+                            <h3><?= substrwords(get_cat_name($cat_id), 15) ?></h3>
+                        </a>
+                    </div>
+                    <div class="col-md-12 img-card-lcs">
+                        <?php
+                            $posts_last = get_posts(array("cat" => $cat_id, "showposts" => 1));
+                            $id_last = $posts_last[0]->ID;
+                        ?>
+                        <div class="card-lcs-last-post">
+                            <a class="card-lcs-link" href="<?= get_category_link($cat_id) ?>">
+                                <?php if (has_post_thumbnail($id_last)): ?>
+                                    <?= get_the_post_thumbnail($id_last, 'thumbnail', array( 'class' => 'col-md-12' )); ?>
+                                <?php else: ?>
+                                    <img class="col-md-12" src="<?= plugin_dir_url(__FILE__) ?>default.jpg">
+                                <?php endif; ?>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    <?php endforeach; ?>
+    </div>
+</div>
